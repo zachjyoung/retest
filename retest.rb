4fcc0ee79fe8b5a59990d3ea26313736811442e4
Zach Young <zachjyoung@gmail.com>
2013-09-24 12:33:36 -0400

    moar code

--- retest.rb
+++ retest.rb
@@ -13,3 +13,17 @@ def even_or_odd
     end
   end
 end
+
+def odd_even_generator
+  yes_no = 'y'
+  until yes_no == 'n' do 
+    puts "please enter a number"
+    puts "That is a #{even_or_odd} number"
+    puts "would you like to provide another number?(y/n)"
+    yes_no = gets.chomp
+  end
+  puts "Odd number (#{@odd_arr.length} total): #{@odd_arr * ","}"
+  puts "Even Numbers (#{@even_arr.length} total): #{@even_arr * ","}"
+end
+
+odd_even_generator
